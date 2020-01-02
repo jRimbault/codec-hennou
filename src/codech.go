109ea78c9e8c30debee71d844c8d7521e43a2c2c
jRimbault <jacques.rimbault@gmail.com>
2020-01-02 17:16:45 +0100

    exploration

--- src/codech.go
+++ src/codech.go
@@ -23,12 +23,17 @@ func work(task func([]byte) []byte, source string, dest string) {
 	if err != nil {
 		panic(err)
 	}
-	ioutil.WriteFile(dest, task(content), 0644)
+	var res []byte = task(content)
+	// parallel split task between chunks
+	// for _, chunk := range split(content, 8) {
+	// 	res = append(res, task(chunk)...)
+	// }
+	ioutil.WriteFile(dest, res, 0644)
 }
 
 func encode(matrix Matrix, stream []byte) []byte {
 	const MASK = uint8(0x0F)
-	res := make([]byte, 0)
+	var res []byte
 	for _, b := range stream {
 		res = append(res, matrix[b&MASK])
 		res = append(res, matrix[(b>>4)&MASK])
@@ -37,7 +42,7 @@ func encode(matrix Matrix, stream []byte) []byte {
 }
 
 func decode(reverse ReverseMatrix, stream []byte) []byte {
-	res := make([]byte, 0)
+	var res []byte
 	for i := 0; i < len(stream); i += 2 {
 		a, b := stream[i], stream[i+1]
 		pos1, pos2 := reverse[a], reverse[b]<<4
@@ -52,4 +57,17 @@ func decode(reverse ReverseMatrix, stream []byte) []byte {
 func getChunkSize(length int) int {
 	// runtime.NumCPU()
 	return 2
+}
+
+func split(buf []byte, lim int) [][]byte {
+	var chunk []byte
+	chunks := make([][]byte, 0, len(buf)/lim+1)
+	for len(buf) >= lim {
+		chunk, buf = buf[:lim], buf[lim:]
+		chunks = append(chunks, chunk)
+	}
+	if len(buf) > 0 {
+		chunks = append(chunks, buf[:len(buf)])
+	}
+	return chunks
 }
