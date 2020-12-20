a4838a1aeff0017b57681c84767c5a86078d7941
jRimbault <jacques.rimbault@gmail.com>
2020-12-20 05:05:46 +0100

    bad

--- src/Program.cs
+++ src/Program.cs
@@ -32,8 +32,10 @@ namespace codech
         )
         {
             const int MAX_SIZE = 4096;
-            using (FileStream reader = new(opts.Source, FileMode.Open))
-            using (FileStream writer = new(opts.Dest, FileMode.Create))
+            using (FileStream innerReader = new(opts.Source, FileMode.Open))
+            using (FileStream innerRriter = new(opts.Dest, FileMode.Create))
+            using (BufferedStream reader = new(innerReader))
+            using (BufferedStream writer = new(innerRriter))
             {
                 var buffer = new byte[MAX_SIZE];
                 while (reader.Read(buffer, 0, buffer.Length) > 0)
@@ -41,6 +43,43 @@ namespace codech
                     var result = worker(buffer).ToArray();
                     writer.Write(result, 0, result.Length);
                 }
+            }
+        }
+
+        private static void BufferedWork(
+            Options opts,
+            Func<IEnumerable<byte>, IEnumerable<byte>> worker
+        )
+        {
+            const int MAX_SIZE = 4096;
+            using (FileStream innerReader = new(opts.Source, FileMode.Open))
+            using (FileStream innerRriter = new(opts.Dest, FileMode.Create))
+            using (BufferedStream reader = new(innerReader))
+            using (BufferedStream writer = new(innerRriter))
+            {
+                var readBuffer = new byte[MAX_SIZE];
+                var writeBuffer = new byte[opts.Encode ? MAX_SIZE * 2 : MAX_SIZE / 2];
+                while (reader.Read(readBuffer, 0, readBuffer.Length) > 0)
+                {
+                    foreach (var (i, b) in worker(readBuffer).Enumerate())
+                    {
+                        writeBuffer[i] = b;
+                    }
+                    writer.Write(writeBuffer, 0, writeBuffer.Length);
+                }
+            }
+        }
+    }
+
+    internal static class LinqExtension
+    {
+        public static IEnumerable<(int, T)> Enumerate<T>(this IEnumerable<T> enumerable)
+        {
+            var i = 0;
+            foreach (var item in enumerable)
+            {
+                yield return (i, item);
+                i += 1;
             }
         }
     }
